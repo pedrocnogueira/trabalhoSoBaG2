07fc790330783fd5adc2e4a663b516c1dae40bd2
pedro.nogueira <pedro.crnogueira@gmail.com>
2024-11-11 13:42:22 -0300

    setup

---
+++ cria_func.c
@@ -0,0 +1,16 @@
+/* Guilherme Ferreira de Ávila - 2311153 */
+/* Pedro Carneiro Nogueira - 2310540 */
+
+typedef enum {INT_PAR, PTR_PAR} TipoValor;
+typedef enum {PARAM, FIX, IND} OrigemValor;
+
+typedef struct {
+   TipoValor    tipo_val;  /* indica o tipo do parametro (inteiro ou ponteiro) */
+   OrigemValor  orig_val;  /* indica a origem do valor do parametro */
+   union {
+     int v_int;
+     void* v_ptr;
+   } valor;         /* define o valor ou endereço do valor do parametro (quando amarrado/indireto) */
+} DescParam;
+
+
